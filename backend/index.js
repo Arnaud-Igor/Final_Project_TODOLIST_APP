450958bc86032061f1fa384517382a255a3da9e3
MacBook Pro <macbookpro@192.168.1.3>
2024-07-24 14:30:08 +0000

    ajout

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 // to make input as json
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ["https://mern-notes-app-ui.vercel.app"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 // app.use(cors());
 
 app.listen(3000, () => {
