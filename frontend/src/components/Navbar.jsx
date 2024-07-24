e9863bc263826415259b34b36db2c2ea5ee1da56
MacBook Pro <macbookpro@192.168.1.3>
2024-07-24 15:53:50 +0000

    ajout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
     try {
       dispatch(signoutStart());
 
-      const res = await axios.get("https://mern-notes-app-api.vercel.app/auth/signout", {
+      const res = await axios.get("https://mern-notes-app-api.vercel.app/api/auth/signout", {
         withCredentials: true,
       });
 
