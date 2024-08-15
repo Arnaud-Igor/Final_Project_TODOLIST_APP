b2f781de78793f7b59f335b39792e3fdbb3521c2
MacBook Pro <macbookpro@192.168.1.4>
2024-08-15 15:44:54 +0000

    modifications

--- backend/utils/verifyUser.js
+++ backend/utils/verifyUser.js
@@ -1,5 +1,7 @@
 import { errorHandler } from "./error.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
